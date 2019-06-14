640b7cf4b87f0610dfa61b5b4d2bc3597fd6d3f9
tallurip <49661895+tallurip@users.noreply.github.com>
2019-06-14 18:58:44 -0400

    Create Praveenraja_Talluri_Sendmoney.py

---
+++ Praveenraja_Talluri_Sendmoney.py
@@ -0,0 +1,19 @@
+import bankdetails
+class sendmoney:
+
+    #find and add the user using firstname, lastname, email and optional mobile number
+    def findandadduser(firstname, lastname, email, mobilenumber = None):
+        return search.user(raja, Talluri, tallurip@mail.sacredheart.edu)
+
+    #check if the amount entered by the user is available or not
+    def checkbalance(amount,owner,recepient):
+        remainingbalance = owner.balance - amount
+        if(remainingbalance >= amount):
+            send(amount,user,recepient)
+        else:
+            print("insufficient funds")
+
+    #send money to the recepient and deduct the money from owner account
+    def send(amount, owner, recepient):
+        recepient.total = recepient.funds + amount
+        owner.total = owner.funds - amount
