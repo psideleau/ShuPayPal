5d9b259f132e7a59b5378a5071fe8f4a5eaf91f0
tallurip <49661895+tallurip@users.noreply.github.com>
2019-06-21 00:30:06 -0400

    Delete setBalance.py

--- rishav_Set_Monthly_bugget/Final Python project/setBalance.py
+++
@@ -1,9 +0,0 @@
-from credential import user
-from credential import Credentials
-
-class give_value:
-    def _init_(self,username,value):
-        self.username=username
-        self.value=value
-
-    
